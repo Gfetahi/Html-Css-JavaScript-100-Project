151a178828ec5cd4a14028e01b14fe34ba99b2a0
Gfetahi <ganiemifetahi@gmail.com>
2025-02-05 17:18:21 +0100

    Fade Out Project Added

---
+++ 22.Fade Out/app.js
@@ -0,0 +1,11 @@
+const arrow = document.getElementById('arrow');
+
+window.addEventListener('scroll', () => {
+    let position = window.scrollY;
+    // console.log(position);
+    if (position <= 5) {
+        arrow.classList.toggle('fade-in')
+        arrow.classList.toggle('fade-out');
+    }
+    
+})
